5c9965f452e40547cc9b6624e28cd58ae8a9a0cd
RaweNist <116841332+RaweNist@users.noreply.github.com>
2026-04-05 23:34:14 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,10 +1,53 @@
 // Copyright 2021 NNTU-CS
-int countPairs1(int *arr, int len, int value) {
-  return 0;
+int countPairs1(int* arr, int len, int value) {
+	int count = 0;
+	for (int i = 0; i < len; ++i) {
+		for (int j = i + 1; j < len; ++j) {
+			if (arr[i] + arr[j] == value)
+				count++;
+		}
+	}
+	return count;
 }
-int countPairs2(int *arr, int len, int value) {
-  return 0;
+int countPairs2(int* arr, int len, int value) {
+	int count = 0;
+	for (int i = 0; i < len/2; ++i) {
+		for (int j = len; j > i; --j) {
+			if (arr[i] + arr[j] == value)
+				count++;
+		}
+	}
+	return count;
 }
-int countPairs3(int *arr, int len, int value) {
-  return 0;
+int countPairs3(int* arr, int len, int value) {
+	int count = 0;
+	for (int i = 0; i < len; ++i) {
+		int x = arr[i];
+		int l = i + 1;
+		int r = len - 1;
+		do {
+			int n = (l + r) / 2;
+			if (arr[n] + x == value) {
+				count++;
+				int n1 = n;
+				n++;
+				while (arr[n] + x == value) {
+					count++;
+					n--;
+				}
+				while (arr[n1] + x == value) {
+					count++;
+					n1++;
+				}
+				break;
+			}
+			if (arr[n] + x <= value) {
+				l = n + 1;
+			}
+			else if (arr[n] + x >= value) {
+				r = n - 1;
+			}
+		} while (l <= r);
+	}
+	return count;
 }
