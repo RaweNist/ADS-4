5f3384b8af6a838908c4ee6b7a722a12f0865344
RaweNist <116841332+RaweNist@users.noreply.github.com>
2026-04-06 11:26:55 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,24 +1,18 @@
 // Copyright 2021 NNTU-CS
 int countPairs1(int* arr, int len, int value) {
   int count = 0;
-  for (int i = 0; i < len; ++i) {
-    for (int j = i + 1; j < len; ++j) {
-      if (arr[i] + arr[j] == value) {
+  for (int i = 0; i < len; ++i)
+    for (int j = i + 1; j < len; ++j)
+      if (arr[i] + arr[j] == value)
         count++;
-      }
-    }
-  }
   return count;
 }
 int countPairs2(int* arr, int len, int value) {
   int count = 0;
-  for (int i = 0; i < len/2; ++i) {
-    for (int j = len; j > i; --j) {
-      if (arr[i] + arr[j] == value) {
+  for (int i = 0; i < len/2; ++i)
+    for (int j = len; j > i; --j)
+      if (arr[i] + arr[j] == value)
         count++;
-      }
-    }
-  }
   return count;
 }
 int countPairs3(int* arr, int len, int value) {
