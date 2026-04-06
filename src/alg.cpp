c170fa8ccd38ea6ea3c03d9e2fb4db0cab89edc7
RaweNist <116841332+RaweNist@users.noreply.github.com>
2026-04-06 18:20:08 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,20 +2,34 @@
 #include "alg.h"
 
 int countPairs1(int* arr, int len, int value) {
-  int count = 0;
-  for (int i = 0; i < len; ++i)
+  for (int i = 0; i < len; ++i) {
     for (int j = i + 1; j < len; ++j)
-      if (arr[i] + arr[j] == value)
+      if (arr[i] + arr[j] == value) {
         count++;
+        while (j + 1 < len && arr[j] == arr[j+1]) {
+          j++;
+        }
+      }
+    while (i + 1 < len && arr[i] == arr[i + 1]) {
+      i++;
+    }
+  }
   return count;
 }
 int countPairs2(int* arr, int len, int value) {
   int count = 0;
-  for (int i = 0; i < len/2; ++i)
+  for (int i = 0; i < len; ++i) {
     for (int j = len; j > i; --j)
-      if (arr[i] + arr[j] == value)
+      if (arr[i] + arr[j] == value) {
         count++;
-  return count;
+        while (arr[j] == arr[j - 1] && j > i) {
+          j--;
+        }
+      }
+    while (i + 1 < len && arr[i] == arr[i + 1]) {
+      i++;
+    }
+  }
 }
 int countPairs3(int* arr, int len, int value) {
   int count = 0;
@@ -30,14 +44,12 @@ int countPairs3(int* arr, int len, int value) {
         int a = n - 1;
         if (a >= l) {
           while (arr[a] == arr[n]) {
-            count++;
             a--;
           }
         }
         int b = n + 1;
         if (b <= r) {
           while (arr[b] == arr[n]) {
-            count++;
             b++;
           }
         }
@@ -48,6 +60,9 @@ int countPairs3(int* arr, int len, int value) {
         r = n - 1;
       }
     } while (l <= r);
+    while (i + 1 < len && arr[i] == arr[i + 1]) {
+      i++;
+    }
   }
   return count;
 }
